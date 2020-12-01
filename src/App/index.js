e36e35ddb34c013be42353ed4c65ab80aa08954c
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-30 19:03:36 -0600

    set up routing.

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/NavBar';
+import Routes from '../helpers/Routes';
 
 class App extends React.Component {
   render() {
     return (
       <>
       <div className="App">
+        <Router>
+          <Navbar />
+          <Routes />
+        </Router>
       </div>
       </>
     );
